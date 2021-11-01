e07da8b78cb5300c23121299677cce5ccdc6305a
Ciara Weston <88999595+ciaraweston@users.noreply.github.com>
2021-11-01 11:34:08 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-  'url': 'mongodb+srv://cweston0727:RIpcpqZAFy0W9qQu@cluster0.ahgdx.mongodb.net/todolist?retryWrites=true&w=majority',
-  'dbName': 'todo'
+   url: 'mongodb+srv://cweston0727:RIpcpqZAFy0W9qQu@cluster0.ahgdx.mongodb.net/todolist?retryWrites=true&w=majority',
+  dbName: 'todo'
 };
